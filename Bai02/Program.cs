033c31f96e448229a4925cea29b2a70ca00a92fd
mavisphung <huypc2410@gmail.com>
2021-03-06 11:32:41 +0700

    Bai02 added

---
+++ Bai02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace Bai02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb1 = new StringBuilder("Tâm");
+            StringBuilder sb2;
+            sb2 = sb1;
+            sb1 = null;
+        }
+    }
+}
