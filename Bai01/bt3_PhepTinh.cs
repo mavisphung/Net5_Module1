f49b7236c58cc99ec33b8829c82c31df5c589899
mavisphung <huypc2410@gmail.com>
2021-03-06 09:48:42 +0700

    Added 3 exercises

---
+++ Bai01/bt3_PhepTinh.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai01
+{
+    class bt3_PhepTinh
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Nhập số a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập số b: ");
+            int b = int.Parse(Console.ReadLine());
+            var tong = a + b;
+            Console.WriteLine($"Tổng = {tong}");
+        }
+    }
+}
